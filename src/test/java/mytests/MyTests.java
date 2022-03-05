78222880ca9f1a2130b02326bc03636f1592d9b4
shankarchanna9 <shankarchanna9@github.com>
2022-03-05 22:25:16 +0530

    Initial commit

---
+++ src/test/java/mytests/MyTests.java
@@ -0,0 +1,34 @@
+package mytests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Analyzer.RetryAnalyzer;
+
+public class MyTests {
+
+	
+	
+	@Test
+	public void Test1()
+	{
+		Assert.assertEquals(false, true);
+	}
+ 
+	@Test
+	public void Test2()
+	{
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void Test3()
+	{
+		
+		Assert.assertEquals(true, true);
+	}
+	
+	
+	
+	
+	
+}
